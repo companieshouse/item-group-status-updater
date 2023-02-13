fbb48e01dbd053f3affb5e578b3e7c6ef2ba015d
mcolwellch <mcolwell@companieshouse.gov.uk>
2023-02-13 11:57:12 +0000

    Added item-group-status-updater skeleton app

---
+++ src/main/java/uk/gov/companieshouse/itemgroupstatusupdater/ItemGroupStatusUpdaterApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.itemgroupstatusupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemGroupStatusUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemGroupStatusUpdaterApplication.class, args);
+	}
+
+}
