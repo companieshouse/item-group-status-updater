07f4c68892c9a4f17031fab21a4b9b87acefc5b8
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-12-19 10:57:42 +0000

    DCAC-216: Remove temporary test code.

--- src/main/java/uk/gov/companieshouse/itemgroupstatusupdater/config/ApplicationConfiguration.java
+++ src/main/java/uk/gov/companieshouse/itemgroupstatusupdater/config/ApplicationConfiguration.java
@@ -1,9 +1,7 @@
 package uk.gov.companieshouse.itemgroupstatusupdater.config;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.gov.companieshouse.itemgroupstatusupdater.service.PatchOrderedItemService;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
@@ -15,16 +13,6 @@ public class ApplicationConfiguration {
     @Bean
     Logger getLogger(){
         return LoggerFactory.getLogger(APPLICATION_NAMESPACE);
-    }
-
-    // TODO DCAC-216: Remove this temporary test code.
-    @Bean
-    CommandLineRunner testServiceOnStartUp(final PatchOrderedItemService service) {
-        return args -> service.patchOrderedItem(
-            "ORD-844016-962315",
-            "CCD-289716-962308",
-            "satisfied",
-            "s3://document-api-images-cidev/docs/--EdB7fbldt5oujK6Nz7jZ3hGj_x6vW8Q_2gQTyjWBM/application-pdf");
     }
 
 }
