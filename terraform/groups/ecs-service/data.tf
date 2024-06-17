e873c5a6f95cd3162fe36d47d83bceb0e1e686a5
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2024-06-17 17:18:17 +0100

    test: use private lb

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -34,7 +34,7 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
 }
 
 data "aws_lb" "service_lb" {
-  name = "${var.environment}-chs-apichgovuk"
+  name = "${var.environment}-chs-internalapi"
 }
 
 data "aws_lb_listener" "service_lb_listener" {
@@ -42,14 +42,6 @@ data "aws_lb_listener" "service_lb_listener" {
   port = 443
 }
 
-data "aws_lb" "secondary_lb" {
-  name = "${var.environment}-chs-apichgovuk-private"
-}
-
-data "aws_lb_listener" "secondary_lb_listener" {
-  load_balancer_arn = data.aws_lb.secondary_lb.arn
-  port = 443
-}
 # retrieve all secrets for this stack using the stack path
 data "aws_ssm_parameters_by_path" "secrets" {
   path = "/${local.name_prefix}"
