b07060fcd23d779370a05a82e06f426380d2e4e8
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2024-11-07 11:59:10 +0000

    feat: bump the tf module version

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.287"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.293"
 
   # Environmental configuration
   environment             = var.environment
@@ -62,6 +62,7 @@ module "ecs-service" {
   use_capacity_provider              = var.use_capacity_provider
   use_fargate                        = var.use_fargate
   fargate_subnets                    = local.application_subnet_ids
+  read_only_root_filesystem          = false
 
   # Cloudwatch
   cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
@@ -82,7 +83,7 @@ module "ecs-service" {
   eric_secrets              = local.eric_secrets
 }
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.287"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.293"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
